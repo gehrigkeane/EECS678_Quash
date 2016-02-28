df32c16259db1c6efa3b4c8459d3bdfd8bee43b6
Gehrig Keane <gehrigkeane@gmail.com>
2016-02-28 17:05:51 -0600

    finished command tokenization and main structure, now we start the real dev

--- quash.c
+++ quash.c
@@ -55,11 +55,47 @@ bool get_command(command_t* cmd, FILE* in) {
 		}
 		else
 			cmd->cmdlen = len;
-		
+
+		////////////////////////////////////////////////////////////////////////////////
+		// Empty Command return true - MUST BE HANDLED
+		////////////////////////////////////////////////////////////////////////////////
+		if (!(int)cmd->cmdlen)
+			return true;
+
+		////////////////////////////////////////////////////////////////////////////////
+		// Tokenize command arguments
+		////////////////////////////////////////////////////////////////////////////////
+		cmd->tok = malloc( sizeof(char*) * MAX_COMMAND_ARGLEN );
+		cmd->toklen = 0;
+
+		char * temp = cmd->cmdstr;
+		temp = strtok (cmd->cmdstr," ");
+		while (temp != NULL)
+		{
+			//debug print - printf ("%d: %s\n", (int)cmd->toklen, temp);
+			cmd->tok[cmd->toklen] = temp;
+			cmd->toklen++;
+			temp = strtok (NULL, " ");
+		}
+
+		////////////////////////////////////////////////////////////////////////////////
+		// Remove NULL token from end
+		////////////////////////////////////////////////////////////////////////////////
+		cmd->toklen--;
+
 		return true;
 	}
 	else
 		return false;
+}
+
+void print_init() {
+	////////////////////////////////////////////////////////////////////////////////
+	// Print Current Working Dir before each command
+	////////////////////////////////////////////////////////////////////////////////
+	char cwd[MAX_COMMAND_LENGTH];		//cwd arg - print before each shell command
+	if ( getcwd(cwd, sizeof(cwd)) )
+		printf("%s $ ", cwd);
 }
 
 /**
@@ -70,23 +106,73 @@ bool get_command(command_t* cmd, FILE* in) {
 	* @return program exit status
  */
 int main(int argc, char** argv) { 
-	command_t cmd; //< Command holder argument
-	
+
+	////////////////////////////////////////////////////////////////////////////////
+	// Init Signal Masking
+	////////////////////////////////////////////////////////////////////////////////
+	sigemptyset(&mask);
+	sigaddset(&mask, SIGCHLD);
+
+	////////////////////////////////////////////////////////////////////////////////
+	// Input stems from FILE
+	////////////////////////////////////////////////////////////////////////////////
+	if ( !isatty( (fileno(stdin) ) ) ) {
+		//Detect quash execution with redirected input
+		//We'll deal with this later
+		return 0;
+	}
+
+	////////////////////////////////////////////////////////////////////////////////
+	// Args
+	////////////////////////////////////////////////////////////////////////////////
+	command_t cmd;						//< Command holder argument
+
 	start();
 	
 	puts("Welcome to Quash!");
 	puts("Type \"exit\" to quit");
+	print_init();
 
 	// Main execution loop
 	while (is_running() && get_command(&cmd, stdin)) {
-		// NOTE: I would not recommend keeping anything inside the body of
-		// this while loop. It is just an example.
 
 		// The commands should be parsed, then executed.
-		if (!strcmp(cmd.cmdstr, "exit"))
+		if (!strcmp(cmd.cmdstr, "exit") || !strcmp(cmd.cmdstr, "quit"))
 			terminate(); // Exit Quash
-		else 
-			puts(cmd.cmdstr); // Echo the input string
+		else if (!cmd.cmdlen) {
+			print_init();
+			continue; 
+		}
+		else if (strcmp(cmd.tok[0], "cd") == 0) {
+			//TODO: IMPLEMENT CD FUNCTION HERE
+			printf("IMPLEMENT CD FUNCTION\n");
+		}
+		else if (strcmp(cmd.tok[0], "echo") == 0) {
+			//TODO: IMPLEMENT ECHO FUNCTION HERE
+			printf("IMPLEMENT ECHO FUNCTION\n");
+		}
+		else if (strcmp(cmd.tok[0], "jobs") == 0) {
+			//TODO: IMPLEMENT JOBS FUNCTION HERE
+			printf("IMPLEMENT JOBS FUNCTION\n");
+		}
+		else if (strcmp(cmd.tok[0], "kill") == 0) {
+			//TODO: IMPLEMENT KILL FUNCTION HERE
+			printf("IMPLEMENT KILL FUNCTION\n");
+		}
+		else if (strcmp(cmd.tok[0], "set") == 0) {
+			//TODO: IMPLEMENT SET FUNCTION HERE
+			printf("IMPLEMENT SET FUNCTION\n");
+		}
+		else {
+			//TODO: IMPLEMENT EXECUTE COMMAND FUNCTION
+			int i = 0;
+			puts("Struct Token String\n");
+			for (;i <= cmd.toklen; i++) {
+				printf ("%d: %s\n", i, cmd.tok[i]);
+			}
+		}
+
+		print_init();
 	}
 
 	return EXIT_SUCCESS;
--- quash.h
+++ quash.h
@@ -7,31 +7,33 @@
 #ifndef QUASH_H
 #define QUASH_H
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <stdbool.h>
-#include <string.h>
 #include <errno.h>
-#include <unistd.h>
 #include <fcntl.h>
 #include <signal.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
 /**
 	* Specify the maximum number of characters accepted by the command string
 	*/
 #define MAX_COMMAND_LENGTH (1024)
+#define MAX_COMMAND_ARGLEN (32)
 
 /**
 	* Holds information about a command.
 	*/
 typedef struct command_t {
+		char** tok;							//tokenized command
 		char cmdstr[MAX_COMMAND_LENGTH];	///< character buffer to store the
 											///< command string. You may want
 											///< to modify this to accept
 											///< arbitrarily long strings for
 											///< robustness.
 		size_t cmdlen;						///< length of the cmdstr character buffer
-
+		size_t toklen;
 		// Extend with more fields if needed
 } command_t;
 
@@ -57,5 +59,8 @@ void terminate();
 	*  @return True if able to fill #command_t.cmdstr and false otherwise
 	*/
 bool get_command(command_t* cmd, FILE* in);
+
+//Signal Masking definition
+sigset_t mask;
 
 #endif // QUASH_H
