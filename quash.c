1970affd33c37b96c2711d370e4bac010c0a24e6
Gehrig Keane <gehrigkeane@gmail.com>
2016-02-28 14:41:37 -0600

    Updated file formatting to tabs - added all includes to header file

--- quash.c
+++ quash.c
@@ -7,11 +7,9 @@
 /**************************************************************************
  * Included Files
  **************************************************************************/ 
-#include "quash.h" // Putting this above the other includes allows us to ensure
-                   // this file's headder's #include statements are self
-                   // contained.
-
-#include <string.h>
+#include "quash.h"	// Putting this above the other includes allows us to ensure
+					// this file's headder's #include statements are self
+					// contained.
 
 /**************************************************************************
  * Private Variables
@@ -31,65 +29,65 @@ static bool running;
  * Start the main loop by setting the running flag to true
  */
 static void start() {
-  running = true;
+	running = true;
 }
 
 /**************************************************************************
  * Public Functions 
  **************************************************************************/
 bool is_running() {
-  return running;
+	return running;
 }
 
 void terminate() {
-  running = false;
+	running = false;
 }
 
 bool get_command(command_t* cmd, FILE* in) {
-  if (fgets(cmd->cmdstr, MAX_COMMAND_LENGTH, in) != NULL) {
-    size_t len = strlen(cmd->cmdstr);
-    char last_char = cmd->cmdstr[len - 1];
+	if (fgets(cmd->cmdstr, MAX_COMMAND_LENGTH, in) != NULL) {
+		size_t len = strlen(cmd->cmdstr);
+		char last_char = cmd->cmdstr[len - 1];
 
-    if (last_char == '\n' || last_char == '\r') {
-      // Remove trailing new line character.
-      cmd->cmdstr[len - 1] = '\0';
-      cmd->cmdlen = len - 1;
-    }
-    else
-      cmd->cmdlen = len;
-    
-    return true;
-  }
-  else
-    return false;
+		if (last_char == '\n' || last_char == '\r') {
+			// Remove trailing new line character.
+			cmd->cmdstr[len - 1] = '\0';
+			cmd->cmdlen = len - 1;
+		}
+		else
+			cmd->cmdlen = len;
+		
+		return true;
+	}
+	else
+		return false;
 }
 
 /**
- * Quash entry point
- *
- * @param argc argument count from the command line
- * @param argv argument vector from the command line
- * @return program exit status
+	* Quash entry point
+	*
+	* @param argc argument count from the command line
+	* @param argv argument vector from the command line
+	* @return program exit status
  */
 int main(int argc, char** argv) { 
-  command_t cmd; //< Command holder argument
-  
-  start();
-  
-  puts("Welcome to Quash!");
-  puts("Type \"exit\" to quit");
+	command_t cmd; //< Command holder argument
+	
+	start();
+	
+	puts("Welcome to Quash!");
+	puts("Type \"exit\" to quit");
 
-  // Main execution loop
-  while (is_running() && get_command(&cmd, stdin)) {
-    // NOTE: I would not recommend keeping anything inside the body of
-    // this while loop. It is just an example.
+	// Main execution loop
+	while (is_running() && get_command(&cmd, stdin)) {
+		// NOTE: I would not recommend keeping anything inside the body of
+		// this while loop. It is just an example.
 
-    // The commands should be parsed, then executed.
-    if (!strcmp(cmd.cmdstr, "exit"))
-      terminate(); // Exit Quash
-    else 
-      puts(cmd.cmdstr); // Echo the input string
-  }
+		// The commands should be parsed, then executed.
+		if (!strcmp(cmd.cmdstr, "exit"))
+			terminate(); // Exit Quash
+		else 
+			puts(cmd.cmdstr); // Echo the input string
+	}
 
-  return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
